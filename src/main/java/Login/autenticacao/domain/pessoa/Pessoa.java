adac2d23c2b2678ac9e6c58049bae60024956bb4
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2023-08-23 19:44:53 -0300

    Igualando branch's

--- src/main/java/Login/autenticacao/domain/pessoa/Pessoa.java
+++ src/main/java/Login/autenticacao/domain/pessoa/Pessoa.java
@@ -1,13 +1,27 @@
 package Login.autenticacao.domain.pessoa;
 
 import Login.autenticacao.domain.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Table(name = "pessoas")
+@Entity(name = "Pessoa")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode( of= "id")
 public class Pessoa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
 
+    @Embedded
     private Endereco endereco;
 
     public Pessoa(DadosCadastroPessoa dados) {
